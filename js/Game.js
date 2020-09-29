8de04ff0b50667f6d63b580993e9e0f6a12b7b6e
vaibhav <vaibhav.ghanta@gmail.com>
2020-09-29 16:52:59 -0500

    animations

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(car1img);
     car2 = createSprite(300,200);
+    car2.addImage(car2img);
     car3 = createSprite(500,200);
+    car3.addImage(car3img);
     car4 = createSprite(700,200);
+    car4.addImage(car4img);
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +47,13 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background("#c68767");
+      image(trackimg,0,-displayHeight*4,displayWidth,displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 190;
       var y;
 
       for(var plr in allPlayers){
@@ -56,16 +61,19 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 240;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
         cars[index-1].y = y;
 
         if (index === player.index){
-          cars[index - 1].shapeColor = "red";
+          fill("red");
+          ellipse(x,y,60,60)
+
+          //cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
-          camera.position.y = cars[index-1].y
+          camera.position.y = cars[index-1].y;
         }
        
         //textSize(15);
@@ -78,7 +86,13 @@ class Game {
       player.distance +=10
       player.update();
     }
+    if(player.distance>4090){
+      gameState=2;
+    }
 
     drawSprites();
+  }
+  end(){
+    console.log("gameend")
   }
 }
--- sketch.js
+++ sketch.js
@@ -5,11 +5,18 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
+var car1img,car2img,car3img,car4img,trackimg;
 
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
+function preload(){
+  car1img=loadImage("images/car1.png");
+  car2img=loadImage("images/car2.png");
+  car3img=loadImage("images/car3.png");
+  car4img=loadImage("images/car4.png");
+  trackimg=loadImage("images/track.jpg");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +34,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end();
   }
 }
